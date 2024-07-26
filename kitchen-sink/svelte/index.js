7b3be7bd1801cf24642b5d9a14d82659f5bf8540
Erika Cudd <erika@cudd.io>
2024-07-26 18:59:24 +0000

    Clean up

--- kitchen-sink/svelte/index.js
+++ kitchen-sink/svelte/index.js
@@ -4,10 +4,6 @@ import App from './components/App.svelte';
 import { mount } from 'svelte';
 
 // eslint-disable-next-line
-// const app = new App({
-//   target: document.querySelector('#app'),
-// });
-
 const app = mount(App, {
   target: document.querySelector('#app'),
 });
